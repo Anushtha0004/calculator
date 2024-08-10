f1aae63de7c4ba4763a6a1ea1b72b8754533be84
Anushtha Sharma <156092745+Anushtha0004@users.noreply.github.com>
2024-08-10 13:05:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function clearDisplay(){
+  document.getElementById('dispaly').value='';
+}
+
+function appendToDisplay(value) {
+    document.getElementById('display').value +=value;
+    
+}
+
+function calculateResult(){
+    const display=document.getElementById('display');
+    try{
+        display.value=eval(display.value);
+    }catch (e){
+        display.value='Error';
+    }
+    
+}
